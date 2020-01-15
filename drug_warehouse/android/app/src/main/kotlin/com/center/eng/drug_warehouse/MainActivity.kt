efd34529d9dcd7f124c767ef4cf2c186c3d0d6a8
yhya rakmani <yhyasallom@gmail.com>
2020-01-15 16:14:58 +0200

    migrate to androidx

--- drug_warehouse/android/app/src/main/kotlin/com/center/eng/drug_warehouse/MainActivity.kt
+++ drug_warehouse/android/app/src/main/kotlin/com/center/eng/drug_warehouse/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.center.eng.drug_warehouse
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
--- drug_warehouse/lib/main.dart
+++ drug_warehouse/lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  void press()=> print("object");
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  void press() => print("object");
+  var yhya = "yhya";
+  var i = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,14 +23,16 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text("this"),
+            Text(yhya + "${i}"),
             RaisedButton(
               child: Text("a1"),
               onPressed: press,
             ),
             RaisedButton(
               child: Text("a2"),
-              onPressed: () => print('a2'),
+              onPressed: () => setState(() {
+                ++i;
+              }),
             ),
             RaisedButton(
               child: Text("a3"),
